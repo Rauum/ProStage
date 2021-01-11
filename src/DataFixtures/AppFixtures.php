7dcccd033c78652b392e3a2440856ee473ab7d07
Romain Vaché <rvache001@iutbayonne.univ-pau.fr>
2021-01-11 19:37:06 +0100

    Pqremière utilisation de faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,11 +10,24 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $formation1 = new Formation();
-        $formation1->setIntitule("Formation1");
-        $formation1->setNiveau("2° année");
-        $formation1->setVille("Anglet");
-        $manager->persist($formation1);
+        //Creation d'un générateur de données Faker
+        $faker = \Faker\Factory::create('fr_FR');
+
+        $nbFormations = 10;
+
+        for ($i=1; $i < $nbFormations; $i++) {
+          //Creation d'une nouvelle formation
+          $formation1 = new Formation();
+          //Génération d'un nom de formation avec un numéro compris entre 0 99
+          $formation1->setIntitule($faker->regexify('Formation [0-9][1-9]'));
+          //Génération d'un niveau qui est soit 1° ou 2° année
+          $formation1->setNiveau($faker->regexify('[1-2]° année'));
+          //Génération d'une ville
+          $formation1->setVille($faker->city());
+          //Enregistrement du module créé
+          $manager->persist($formation1);
+        }
+
 
         $manager->flush();
     }
